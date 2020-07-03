6ac2ec20daed693b5b034efd60ab75754b35eccf
witchborder <67751125+witchborder@users.noreply.github.com>
2020-07-04 06:53:31 +0700

    Add files via upload

---
+++ SL/resources/js/script.js
@@ -0,0 +1,36 @@
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "150px";
+        document.getElementById("main").style.marginLeft = "150px";  
+    }
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+    }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+    
+    
+
